dc6146ba16d285047b1284ba7b0f6b072652679d
Alex Ge <alexander.ge@bestseller.com>
2019-09-01 14:13:12 +0100

    table view + cell

---
+++ BBCTopStories/BBCTopStories/Headlines/Views/HeadlinesCell.swift
@@ -0,0 +1,13 @@
+//
+//  HeadlinesCell.swift
+//  BBCTopStories
+//
+//  Created by Alexander Ge on 01/09/2019.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+final class HeadlinesCell: UITableViewCell {
+    
+}
--- BBCTopStories/BBCTopStories/Headlines/Views/HeadlinesListViewController.swift
+++ BBCTopStories/BBCTopStories/Headlines/Views/HeadlinesListViewController.swift
@@ -10,4 +10,35 @@ import UIKit
 
 final class HeadlinesListViewController: UIViewController {
     
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
 }
+
+extension HeadlinesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension HeadlinesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "HeadlinesCell") as! HeadlinesCell
+    }
+}
+
+#if DEBUG
+extension HeadlinesListViewController {
+    var exposedTableView: UITableView {
+        return tableView
+    }
+}
+#endif
