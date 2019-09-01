1d7f902e4eb7d372e01ba25ec65cd3067bb464e7
Alex Ge <alexander.ge@bestseller.com>
2019-09-01 12:59:58 +0100

    add kingfisher for image caching

--- BBCTopStories/Pods/Target Support Files/Kingfisher/Kingfisher-umbrella.h
+++ BBCTopStories/Pods/Target Support Files/Kingfisher/Kingfisher-umbrella.h
@@ -10,7 +10,8 @@
 #endif
 #endif
 
+#import "Kingfisher.h"
 
-FOUNDATION_EXPORT double Pods_BBCTopStoriesTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BBCTopStoriesTestsVersionString[];
+FOUNDATION_EXPORT double KingfisherVersionNumber;
+FOUNDATION_EXPORT const unsigned char KingfisherVersionString[];
 
